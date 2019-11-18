e9ff7f6205c7a2336138c1ead20807e061908b7d
Dmitriy Muravev <dmuravev@iiko.ru>
2019-11-18 15:06:53 +0300

    commit1

---
+++ www/precache-manifest.ccf433176e8c5b2b03888c55a868fd66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "968442c9102e4d893f017d7b2861031f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a6f2ac285a863681a0e5",
+    "url": "./static/css/main.dfca195d.chunk.css"
+  },
+  {
+    "revision": "830b02d16e04f13e49ed",
+    "url": "./static/js/2.5b007e28.chunk.js"
+  },
+  {
+    "revision": "a6f2ac285a863681a0e5",
+    "url": "./static/js/main.5ed53990.chunk.js"
+  },
+  {
+    "revision": "4cefd424de3ca8b7976b",
+    "url": "./static/js/runtime-main.0bd261fc.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "./static/media/logo.25bf045c.svg"
+  }
+]);
